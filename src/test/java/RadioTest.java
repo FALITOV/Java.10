6f59253fb126471c6326f7c9adeadc66dac84f7e
Egorius188 <postulgin98@mail.ru>
2023-11-27 18:27:25 +0300

    add test

--- src/test/java/RadioTest.java
+++ src/test/java/RadioTest.java
@@ -1,33 +1,123 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class RadioTest {
-    private int currentStation;
-    private int currentVolume;
-
-    public int getCurrentStation() {
-        return currentStation;
+    @Test
+    public void moreStationThanThereIs() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(10);
+        int expected = radio.getCurrentStation();
+        int actual = radio.getCurrentStation();
+        Assertions.assertEquals(expected, actual);
     }
 
-    public void setCurrentStation(int currentStation) {
-        if (currentStation < 0) {
-            return;
-        }
-        if (currentStation > 9) {
-            return;
-        }
-        this.currentStation = currentStation;
+    @Test
+    public void lessStationThanThereIs() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(-1);
+        int expected = radio.getCurrentStation();
+        int actual = radio.getCurrentStation();
+        Assertions.assertEquals(expected, actual);
     }
 
-    public int getCurrentVolume() {
-        return currentVolume;
+    @Test
+    public void lessVolumeThanThereIs() {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(-1);
+        int expected = radio.getCurrentVolume();
+        int actual = radio.getCurrentVolume();
+        Assertions.assertEquals(expected, actual);
     }
 
-    public void setCurrentVolume(int currentVolume) {
-        if (currentVolume < 0) {
-            return;
-        }
-        if (currentVolume > 100) {
-            return;
-        }
-
-        this.currentVolume = currentVolume;
+    @Test
+    public void moreVolumeThanThereIs() {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(101);
+        int expected = radio.getCurrentVolume();
+        int actual = radio.getCurrentVolume();
+        Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    public void switchesNextStation() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(8);
+        radio.next();
+        int expected = 9;
+        int actual = radio.getCurrentStation();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void switchesPrevStation() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(1);
+        radio.prev();
+        int expected = 0;
+        int actual = radio.getCurrentStation();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void switchesPrevFirstStation() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(0);
+        radio.prev();
+        int expected = 9;
+        int actual = radio.getCurrentStation();
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void switchesNextLastStation() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(9);
+        radio.next();
+        int expected = 0;
+        int actual = radio.getCurrentStation();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void mustIncreaseVolume() {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(11);
+        radio.increaseVolume();
+        int expected = 12;
+        int actual = radio.getCurrentVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void increaseVolumeMoreThanThereIs() {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(100);
+        radio.increaseVolume();
+        int expected = 100;
+        int actual = radio.getCurrentVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void mustDecreaseVolume() {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(100);
+        radio.decreaseVolume();
+        int expected = 99;
+        int actual = radio.getCurrentVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void decreaseVolumeLessThanThereIs() {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(0);
+        radio.decreaseVolume();
+        int expected = 0;
+        int actual = radio.getCurrentVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+
+
 }
